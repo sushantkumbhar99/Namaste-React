e92cd244e8539eebbcf06608d53cf5fa0d896090
sushantkumbhar99 <sushantkumbhar8123@gmail.com>
2024-01-15 10:19:12 +0530

    React added through node modules

--- App.js
+++ App.js
@@ -1,3 +1,16 @@
+
+
+
+
+import React from 'react';
+import  ReactDOM from 'react-dom/client';
+
+
+
+
+
+
+
 const heading = React.createElement("div",{id:"parent"},
   [  React.createElement( "div" ,{id:"child"},
        [ React.createElement("h1",{} ,"This is the inside in the child"),
