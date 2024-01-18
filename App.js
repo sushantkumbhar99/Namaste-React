8147d2c412f5d5308c9b211fab9db31a16423577
sushantkumbhar99 <sushantkumbhar8123@gmail.com>
2024-01-18 21:32:40 +0530

    This is the Assignment no three

--- App.js
+++ App.js
@@ -12,17 +12,64 @@ import  ReactDOM from 'react-dom/client';
     //  const jsxHeading = <h1> Namaste React with sushant</h1>
     const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    const Title = () =>{
-        return <h2> This is the first title of my componen </h2>
-    }
+//     const Title = () =>{
+//         return <h2> This is the first title of my component</h2>
+//     }
+
+
+// const add = 9+9;
+// console.log( add)
+//     const ComponentHeading = () =>  (
+//         <div id='compo'>
+//             <Title/>
+//         {add}
+//          <h1>First component</h1>
+//          </div>
+//     )
 
 
 
-    const ComponentHeading = () =>  (
-        <div id='compo'>
-            <Title/>
-         <h1>First component</h1>
-         </div>
-    )
+//Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class “title”)
+// const title =  React.createElement("div",{className:"title"}, 
+// [React.createElement("h1",{},"This is h1"),
+// React.createElement("h2",{},"This is h2"),
+// React.createElement("h3",{},"This is h3 in react")])
 
-root.render(<ComponentHeading/>); 
+
+//Create the same element using JSX
+// const title =  <div className='title'>
+//     <h1>This is h1 in jsx</h1>
+//     <h2>This is h2 in jsx</h2>
+//     <h3>This is h3 in jsx</h3>
+// </div>
+
+ 
+//Create a functional component of the same with JSX
+
+//Pass attributes into the tag in JSX
+
+
+const add = 9+9;
+const Title = () =>(
+  
+         <div className='title'>
+           <h1>This is h1 in React Component</h1>
+           <h2>This is h2 in React Component</h2>
+           <h3>This is h3 in React Component</h3>
+           {add}
+        </div>
+)
+
+
+//Composition of Component(Add a component inside another)
+
+
+const  Title2 = () => (
+     <div id='title2'>
+        <h1>This is the new component</h1>
+      {<Title/>}  
+      {<Title></Title>}
+     </div>
+)
+
+ root.render(<Title2/>); 
