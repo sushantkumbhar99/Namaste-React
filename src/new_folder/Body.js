f1d41e30fd0cd51ef2409a318ef7e9c2293c5ea0
sushantkumbhar99 <sushantkumbhar8123@gmail.com>
2024-01-30 17:20:19 +0530

    Top rated functionality is added

--- src/new_folder/Body.js
+++ src/new_folder/Body.js
@@ -2,20 +2,33 @@
 import React from "react";
 import resList from "../utils/mockdata";
 import Restrocard from "./Restrocard";
+import { useState } from "react";
+
 
 const Body= () =>{
+
+  const[listofRestaurants, setListofRestaurants] = useState(resList)
+
     return(
       <div className="body">
          <div className="search">
-            <input type="text" placeholder='search here..' />
+         
             <div className="search-buttons">
-           <button className='search-button'>Search</button>
+           <button className='search-button' onClick={()=> {
+              const filteredList = listofRestaurants.filter( (resa)=> resa.info.avgRating >= 4.6);
+                 setListofRestaurants(filteredList);
+                 console.log(filteredList);
+            }} 
+            >Top rated Restaruants</button>
+
            </div>
          </div>
          <div className="res-cards">
        
         {
-          resList.map((restaurant)=> <Restrocard key={restaurant.info.id}   resData={restaurant}/>)
+          listofRestaurants.map((restaurant)=>
+           <Restrocard key={restaurant.info.id}   resData={restaurant}/>
+           )
         }
              
              
--- src/new_folder/Restrocard.js
+++ src/new_folder/Restrocard.js
@@ -1,9 +1,11 @@
 import React from "react"; 
 import { RESTRO_IMG_URL } from "../utils/constant";
+
+
   const Restrocard = (props) => {
     const {resData} = props;
     
-    const{cloudinaryImageId, name, avgRatingString, sla: { slaString },cuisines,areaName }= resData?.info;
+    const{cloudinaryImageId, name, avgRating, sla: { slaString },cuisines,areaName }= resData?.info;
   
  
    return(
@@ -11,7 +13,7 @@ import { RESTRO_IMG_URL } from "../utils/constant";
       <img className='restro-img' src={RESTRO_IMG_URL+ cloudinaryImageId} />
        <div className="res-data"> 
        <h3>{name}</h3>
-       <h5>{avgRatingString} rating</h5>
+       <h5>{avgRating} rating</h5>
        <h5>{slaString} </h5>
        <p>{cuisines.join(",")}</p>
        <p>{areaName}</p>
